007ca0afb574b95d95b6b9c8ed2c12c3df0059fe
ksnts <santosjohnkyllem@gmail.com>
2021-12-26 23:18:33 +0800

    Code 2 complete

---
+++ assignment_2-2.py
@@ -0,0 +1,13 @@
+buyO=int(input("Input amount of oranges: "))
+buyA=int(input("Input amount of apples: "))
+apple=20
+orange=25
+
+print("Note that: Apples: 20 pesos each, Orange: 25 pesos each")
+
+buy1= buyO*orange
+buy2= buyA*apple
+total= buy1+buy2
+
+print(f"Total amount is: {total} pesos ")
+
